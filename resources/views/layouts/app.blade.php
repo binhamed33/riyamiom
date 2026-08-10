03231f9940d94f38598f5d89304fdd085be5b500
Admin <admin@local>
2026-08-10 23:26:59 +0400

    إزالة تكرار زر نص-إلى-إجراء في صفحة القضية - الزر العائم يحمل سياق القضية الحالية تلقائياً

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1309,7 +1309,7 @@
         @if (!auth()->user()->isClient())
             {{-- Speech-to-Action floating button (global) --}}
             <div class="fixed bottom-5 left-5 z-[250] block">
-                <x-nl-action-modal />
+                <x-nl-action-modal :case-id="request()->route()?->parameter('case')?->id" />
             </div>
             <x-doc-viewer />
         @endif
